03a6a778da09a811040c96e43faaa7c61b1570f9
jamesoneill997 <jamesoneill997@gmail.com>
2020-03-25 01:44:41 +0000

    Added day_getter

---
+++ day_getter.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My PHP</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<?php
+		$day = date("w");		
+		$days_of_week = array('Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday','Saturday');
+		echo "<p>It is ".$days_of_week[$day]." </p>";
+
+	?>
+
+</body>
+</html>
